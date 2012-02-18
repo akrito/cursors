19df68e4e2b591f77655e31b4a38a7d025d7f413
Alex Kritikos <alex@8bitb.us>
2012-02-18 11:56:24 -0600

    Return connection subclasses.

--- cursors.py
+++ cursors.py
@@ -1,4 +1,4 @@
-from psycopg2.extensions import adapt
+import psycopg2.extensions
 from psycopg2.extras import NamedTupleCursor
 import collections
 import os
@@ -26,7 +26,7 @@ class PostgresqlCursor(NamedTupleCursor, collections.Sequence):
         return length
 
     def __repr__(self):
-        return "cursors.ListCursor(%s)" % adapt(self.query)
+        return "cursors.ListCursor(%s)" % psycopg2.extensions.adapt(self.query)
 
     def __str__(self):
         return self.__repr__()
@@ -65,36 +65,42 @@ class SQLiteCursor(sqlite3.Cursor,  collections.Sequence):
 
 # Connections
 
-class PostgresqlConnection(object):
+class PostgresqlConnection(psycopg2.extensions.connection):
     
-    def __init__(self, **kwargs):
-        if 'user' not in kwargs:
-            kwargs['user'] = 'postgres'
-        self.con = psycopg2.connect('dbname=%s user=%s' % (kwargs['name'], kwargs['user']))
-
-    def cursor_execute(self, q, *params):
+    def __call__(self, q, *params):
         if q.upper().startswith('SELECT'):
             # Server-side cursors are cool. Unless we know better, use them for everything.
-            cursor = self.con.cursor(str(uuid.uuid1()), cursor_factory=PostgresqlCursor)
+            cursor = self.cursor(str(uuid.uuid1()))
         else:
-            cursor = self.con.cursor()
+            cursor = self.cursor()
         cursor.execute(q, params)
         return cursor
 
+    def cursor(self, name=None, cursor_factory=PostgresqlCursor, **kwargs):
+        kwargs['cursor_factory'] = cursor_factory
+        if name is not None:
+            kwargs['name'] = name
+        return super(PostgresqlConnection, self).cursor(**kwargs)
 
-class SQLiteConnection(object):
 
-    def __init__(self, name):
-        self.con = sqlite3.connect(name)
-        self.con.row_factory = self.namedtuple_factory
+class SQLiteConnection(sqlite3.Connection):
 
-    def cursor_execute(self, q, *params):
-        cursor = self.con.cursor(SQLiteCursor)
-        cursor.execute(q, params)
-        cursor.query = q
-        return cursor
+    def __init__(self, *args, **kwargs):
+        super(SQLiteConnection, self).__init__(*args, **kwargs)
+        self.use_undocumented_c_method = False
+        self.row_factory = self._row_factory
 
-    def namedtuple_factory(self, cursor, row):
+    def __call__(self, q, *params):
+        if self.use_undocumented_c_method:
+            return super(SQLiteConnection, self).__call__(q, *params)
+        else:
+            self.use_undocumented_c_method = True
+            return self.cursor().execute(q, params)
+
+    def cursor(self, factory=SQLiteCursor):
+        return super(SQLiteConnection, self).cursor(factory)
+
+    def _row_factory(self, cursor, row):
         """
         Usage:
         con.row_factory = namedtuple_factory
@@ -105,17 +111,25 @@ class SQLiteConnection(object):
         return Row(*row)
 
 
-class D(object):
-    """
-    Callable database connection. Call it and you get a cursor.
-    
-    """
+def connect(*args, **kwargs):
+    engine = kwargs.get('engine', None)
 
-    def __init__(self, name='', engine=None, **kwargs):
-        if engine == 'sqlite' or (engine is None and os.path.exists(name)):
-            self.con = SQLiteConnection(name)
+    # If no engine was specified, try to guess what to do.
+    if engine is None and args:
+        if args[0] == ':memory:' or os.path.exists(args[0]):
+            engine = 'sqlite'
         else:
-            self.con = PostgresqlConnection(name, **kwargs)
+            engine = 'postgresql'
+            kwargs.setdefault('database', args[0])
+            kwargs.setdefault('user', 'postgres')
+            args = []
 
-    def __call__(self, q, *params):
-        return self.con.cursor_execute(q, *params)
+    if engine in ('sqlite', 'sqlite3'):
+        kwargs.setdefault('factory', SQLiteConnection)
+        return sqlite3.connect(*args, **kwargs)
+
+    if engine in ('pg', 'postgres', 'postgresql', 'pscyopg', 'psycopg2'):
+        kwargs.setdefault('connection_factory', PostgresqlConnection)
+        return psycopg2.connect(*args, **kwargs)
+
+        
